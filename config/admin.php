8eadd48ea275b7b3401c84c39912a06d3c416212
Arnoldo Sanchez <arnoldo.sanchez.reyes.13@gmail.com>
2023-09-28 16:10:14 -0600

    removiendo iconos documento admin

--- config/admin.php
+++ config/admin.php
@@ -22,8 +22,8 @@ $pendiente = $sql->fetchAll(PDO::FETCH_ASSOC);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dulce Vita</title>
-    <link rel="shortcut icon" href="https://firebasestorage.googleapis.com/v0/b/base-menu-869e3.appspot.com/o/dulcevita%2Flogo.png?alt=media&token=0e6746e2-aef2-4752-8090-e63ca5bcd4b3">
+    <title>Env Inventario</title>
+    <link rel="shortcut icon" href="">
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -182,7 +182,7 @@ $pendiente = $sql->fetchAll(PDO::FETCH_ASSOC);
     <!-- Copyright -->
     <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
         © 2022 Copyright:
-        <a class="text-white" href="index.php">Dulce Vita Tab</a>
+        <a class="text-white" href="index.php"></a>
     </div>
     <!-- Copyright -->
 
